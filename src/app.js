34c176825e1d349bf095981b0f5c83822201520d
Giuseppe Albrizio <g.albrizio@gmail.com>
2021-10-27 10:27:54 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ import mongoDbConfig from './config/mongodb.config';
 /**
  * Import: Error handlers
  */
-import { NotFoundError } from './errors/old/errors.helper';
+import { NotFoundError } from './errors';
 import errorHandler from './middlewares/errorHandler.middleware';
 
 /**
