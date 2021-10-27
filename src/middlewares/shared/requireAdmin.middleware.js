d647d8d3630ae4c684a01a70ae8a4dbefd46011a
Giuseppe Albrizio <g.albrizio@gmail.com>
2021-10-27 16:45:02 +0200

    Update README

--- src/middlewares/shared/requireAdmin.middleware.js
+++ src/middlewares/shared/requireAdmin.middleware.js
@@ -1,9 +1,7 @@
 import { ApplicationError } from '../../errors';
 
-// eslint-disable-next-line import/prefer-default-export
 export const requireAdmin = (req, res, next) => {
-  // console.log(req.currentUser);
-  if (!req.currentUser?.roleAccess?.admin) {
+  if (req.currentUser?.role !== 'admin') {
     throw new ApplicationError(401, 'You are not admin');
   }
   next();
