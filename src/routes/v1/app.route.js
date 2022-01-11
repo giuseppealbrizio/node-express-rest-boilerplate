d178779da4e45e053bd9852052f2643fb653a359
Giuseppe Albrizio <g.albrizio@gmail.com>
2022-01-11 10:52:01 +0100

    - upload service and middleware refactor
    - upload service and middleware refactor

--- src/routes/v1/app.route.js
+++ src/routes/v1/app.route.js
@@ -2,20 +2,18 @@ import express from 'express';
 import { requireAuth } from '@skeldon/sdv3-shared-library';
 import appController from '../../controllers/app.controller';
 import catchAsync from '../../middlewares/catchAsync.middleware';
-import { multerGCSUpload } from '../../middlewares/upload.middleware';
+import { uploadSingleFileToGCS } from '../../middlewares/upload.middleware';
 
 const { getAll, getOne, update, deleteOne } = appController;
 
 const router = express.Router();
 
-router
-  .route('/')
-  .get(requireAuth, multerGCSUpload.single('file'), catchAsync(getAll));
+router.route('/').get(requireAuth, uploadSingleFileToGCS, catchAsync(getAll));
 
 router
   .route('/:id')
-  .get(requireAuth, multerGCSUpload.single('file'), catchAsync(getOne))
-  .patch(requireAuth, multerGCSUpload.single('file'), catchAsync(update))
-  .delete(requireAuth, multerGCSUpload.single('file'), catchAsync(deleteOne));
+  .get(requireAuth, uploadSingleFileToGCS, catchAsync(getOne))
+  .patch(requireAuth, uploadSingleFileToGCS, catchAsync(update))
+  .delete(requireAuth, uploadSingleFileToGCS, catchAsync(deleteOne));
 
 export default router;
